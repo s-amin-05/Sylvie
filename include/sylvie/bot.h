ed3e98c34553e56958b73268e8fb3a79d3428ebe
Amin Solkar <aminsolkar5@gmail.com>
2025-12-09 23:42:49 +0530

    Structure: Added header files for uci and bot

---
+++ include/sylvie/bot.h
@@ -0,0 +1,9 @@
+//
+// Created by amin05 on 12/9/25.
+//
+
+#ifndef SYLVIE_BOT_H
+#define SYLVIE_BOT_H
+
+#endif // SYLVIE_BOT_H
+
---
+++ include/sylvie/uci.h
@@ -0,0 +1,8 @@
+//
+// Created by amin05 on 12/9/25.
+//
+
+#ifndef SYLVIE_UCI_H
+#define SYLVIE_UCI_H
+
+#endif // SYLVIE_UCI_H
